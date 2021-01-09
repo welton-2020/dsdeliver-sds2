4147b8269f66a954790b87e4d1960919f94dfa40
welton-2020 <weltdias@gmail.com>
2021-01-08 23:01:36 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
-    <div >
-     
+    <div>
+      <Navbar />
     </div>
-  );
+   );
 }
 
 export default App;
