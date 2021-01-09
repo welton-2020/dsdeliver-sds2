78955de800b04e8db0a4a54eaadaae54a47cb5b3
welton-2020 <weltdias@gmail.com>
2021-01-09 00:10:20 -0300

    Add Home page

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css'
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer(){
+    return (
+
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/channel/UCySA_C75gY-_fPPp8I7LmFA" target="_new">
+                    <YoutubeIcon />
+                </a>
+
+                <a href="https://www.linkedin.com/in/welton-dias-pereira-6a1b5aa2/" target="_new">
+                    <LinkedinIcon />
+                </a>
+
+                <a href="https://www.instagram.com/welton_dias_2018/?hl=pt-br" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer;
