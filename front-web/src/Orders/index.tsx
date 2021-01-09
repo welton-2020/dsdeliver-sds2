21aeab3b60448ed824c2d636486797d1ea0dff93
welton-2020 <weltdias@gmail.com>
2021-01-09 11:03:48 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import StepsHeader from './StepsHeader'
+import './styles.css'
+
+function Orders(){
+    return (
+
+      <div className="ordes-container"> 
+        <StepsHeader />
+      </div>
+
+    )
+}
+
+export default Orders;
---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Orders from "./Orders";
+import Navbar from "./Navbar";
+
+function Routers(){
+    return(
+        <BrowserRouter>
+                 <Navbar />
+            <Switch>
+                <Route path="/orders"> 
+                    <Orders />
+                </Route>
+                <Route path="/"> 
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+
+}
+
+export default Routers;
