4f5cc4e5b42df4cc5be96fb43688bd614afc4c01
welton-2020 <weltdias@gmail.com>
2021-01-18 10:50:29 -0300

    Order location correction

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -38,7 +38,7 @@ function OrderLocation({ onChangeLocation}: Props) {
               lat: item.center[1],
               lng: item.center[0]
             },
-   //         place: item.place_name,
+           place: item.place_name,
           });
         });
 
